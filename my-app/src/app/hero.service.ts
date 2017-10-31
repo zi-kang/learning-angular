644c627c87a77fd82ac032b570229033d2322172
huzikang <huzikang@sightp.com>
2017-10-31 10:48:27 +0800

    服务

---
+++ my-app/src/app/hero.service.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by huzikang on 17/10/31.
+ */
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
